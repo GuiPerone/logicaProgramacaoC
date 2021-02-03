68917fc28ac289ed8594bf3d95f0428a86dd003e
Guilherme Perone <guilherme.perone@hotmail.com>
2021-02-03 17:14:59 -0300

    Aula 2

---
+++ Aula 02/Aula 22.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+int main () {
+	char nome
+	 int numero 
+	 bool t, f 
+	
+	//Solicitar o nome de usuário da pessoa, se ela não tiver, fará o cadastro
+	printf("Você possui cadastro?\n tecle 1 se tiver e 0 se precisa fazer o cadastro")
+	scanf("%d", t&&f);
+	if ('resposta for 1')
+	 printf("Digite sua letra de usuário");
+	 scanf("%c", nome);
+	 printf("Digite seu número de usuário");
+	 scanf("%d");
+	 
+}
