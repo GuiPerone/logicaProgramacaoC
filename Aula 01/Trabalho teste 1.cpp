7044444617ca2b7b063f0ad46408273e55c43e96
Guilherme Perone <guilherme.perone@hotmail.com>
2021-02-02 22:03:28 -0300

    Aula 01

---
+++ Aula 01/Trabalho teste 1.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<stdlib.h>
+int main ()
+{
+	char x;
+	int y;
+	printf( "Bom dia professora");
+	system("pause");
+	printf("Escreva seu nome");
+	scanf("%f",&x);
+	printf("Escolha um número de 10 a 50");
+	scanf("%d",&y);
+	system("pause");
+	if ("=<10 %d <=50");{
+		printf("Seu número e seu nome são, respectivamente", x&y);
+	} 
+	if else{
+		printf("Escolha um número de 10 a 50")
+		system("pause")
+		sacnf("%d",&y)
+		}
---
+++ Aula 01/teste 1.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<stdlib.h>
+int main ()
+{
+	 char x;
+	int y;
+	printf( "Bom dia professora");
+	system("pause");
+	printf("Escreva seu nome");
+	scanf("seu nome e",&x);
+	printf("Escolha um numero");
+	scanf("Seu numero e",&y);
+	system("pause");
+	printf("O seu número é %d", y, "Seu nome é %f", x);
+	system("pause");
+	
+		}
+
